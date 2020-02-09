03b8691dea11321bd36132c6dc5fccb6e4fe12d8
Kristian Friis <friis1212@gmail.com>
2020-02-09 13:32:04 +0100

    added a module for blog posts so that they can be loaded manually from a JSON file.

---
+++ src/blog_post.mjs
@@ -0,0 +1,21 @@
+const blogPost = [
+  {
+    id: 1,
+    title: 'First Course: Simple Programming',
+    text: 'The first course I took was called programming. I call the blog post for simple programming because it would be more fitting. The course itself is just called “Programming”, but I suspect it is called that because it looks better than “Simple Programming” or “Programming Fundamentals”.'
+          + '<br>' +
+          'It was a 7 week course where we were taught taught fundamental programming in Javascript, which makes a lot of sense considering it is really easy to get started with Javascript. You only need a browser and a text editor then you are good to go.'
+           + '<br>' +
+          'Having already finished almost all of FreeCodeCamps Javascript section, I did not find the course hard or difficult. I did learn some new things which was nice and finally got my head wrapped around multi dimensional arrays.'
+          +'<br>' +
+          'For my exam project I made a simple website that would take the length, width, height and weight of up to 10 different parcels/pallets and would output which carrier service had surcharges on the input and what kind of surcharges. The main things I took away from this project was:'
+          + '<br>' +
+          'Loops: Before this project I used to just hardcode things instead of creating a loop and loop through them. This was a huge timesaver, and cleared up all the many lines of code that did the same thing. It also helped with the readability of my code. So I learned how DRY should be a part of your mindset when programming.'
+          + '<br>' +
+          'Multidimensional Arrays: Really simple actually, I just learned and understood how multidimensional arrays work.'
+          + '<br>' +
+          'Structure: The way I structured this project, which was my first larger project, was just in one .js file which is a horrible way to do it. Since then I have reminded myself over and over to separate files and code in a module like way. This has helped a lot when reading my code later and not get super confused.'
+  }
+];
+
+export default blogPost
---
+++ src/index.mjs
@@ -0,0 +1,7 @@
+import blogPost from './blog_post.mjs';
+
+function test() {
+  console.log(blogPost);
+}
+
+document.AddEventListener('DOMContentLoaded', test())
